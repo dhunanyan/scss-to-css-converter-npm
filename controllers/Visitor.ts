25bcf72cecc9d99aa165ca298ed3b8d664e79508
dhunanyan <davit.hunanyan@me.com>
2024-11-10 16:18:28 +0100

    feat: fix all the bugs related to block, statement, property handling

--- controllers/Visitor.ts
+++ controllers/Visitor.ts
@@ -4,9 +4,11 @@ import {
   BlockContext,
   StatementContext,
   PropertyDeclarationContext,
+  PropertyValueContext,
+  ListSpaceSeparatedContext,
+  ListElementContext,
 } from '@parser/SCSSParser';
 import SCSSParserVisitor from '@parser/SCSSParserVisitor';
-import { TerminalNode } from 'antlr4';
 
 export type DeclarationType = string;
 export type DeclarationStackType = DeclarationType[];
@@ -24,7 +26,8 @@ export class Visitor extends SCSSParserVisitor<void> {
 
   private declarationStack: DeclarationStackType = [];
   private declarationList: DeclarationListType = [];
-  private isLastDeclarationApplied: boolean = false;
+
+  private spaceSeparatedValueStack: string[] = [];
 
   visitRuleset = (ctx: RulesetContext): void => {
     const selectorGroupCtx: SelectorGroupContext = ctx.selectorGroup();
@@ -35,73 +38,99 @@ export class Visitor extends SCSSParserVisitor<void> {
 
     this.visit(blockCtx);
 
-    const isBlockEmpty = blockCtx.children?.length === 2;
-    const blockContainsDeclarations =
-      blockCtx.children?.length === 3 &&
-      blockCtx.children?.some(
-        (c) =>
-          c instanceof StatementContext &&
-          c.children?.some((c) => c instanceof RulesetContext)
-      );
-    if (!isBlockEmpty && !blockContainsDeclarations) {
-      this.selectorList.push([...this.selectorStack]);
-    }
+    this.selectorList.push([...this.selectorStack]);
 
     // removes flat selector for current block (in order to apply to array the new one later)
     this.selectorStack.pop();
   };
 
-  visitStatement = (ctx: StatementContext): void => {
-    ctx.children?.forEach((child) => {
-      if (child instanceof PropertyDeclarationContext) {
-        this.declarationStack.push(`${child.getText()}`);
-        this.visit(child);
-        return;
+  visitListSpaceSeparated = (ctx: ListSpaceSeparatedContext): void => {
+    ctx.children?.forEach((c) => {
+      this.visit(c);
+
+      if (c instanceof ListElementContext) {
+        c.children?.forEach((listElementChild) => {
+          this.spaceSeparatedValueStack.push(listElementChild.getText());
+        });
+      }
+    });
+  };
+
+  visitPropertyDeclaration = (ctx: PropertyDeclarationContext): void => {
+    const propertyDeclaration = ctx.children?.map((c) => {
+      if (c instanceof PropertyValueContext) {
+        return c.children
+          ?.map((propertyValueCtx) => {
+            if (propertyValueCtx instanceof ListSpaceSeparatedContext) {
+              this.spaceSeparatedValueStack = [];
+              this.visit(propertyValueCtx);
+              return this.spaceSeparatedValueStack.join(' ');
+            }
+
+            return propertyValueCtx.getText();
+          })
+          ?.join(' ');
       }
 
-      // @ISSUE-1 this does is not being called for the last Statement of PropertyDeclarationContext instance.
-      // So the last propertyDeclaration item is not being added to array (only the last one)
-      if (this.declarationStack.length > 0) {
-        this.declarationList.push([...this.declarationStack]);
-        this.declarationStack = [];
+      if (c === ctx.Colon()) {
+        return c.getText() + ' ';
       }
-      this.visit(child);
+
+      return c.getText();
     });
+
+    this.declarationStack.push(`${propertyDeclaration?.join('')}`);
   };
 
   visitBlock = (ctx: BlockContext): void => {
-    ctx.children?.forEach((child) => {
-      this.visit(child);
+    const statements = ctx.children?.filter(
+      (c) => c instanceof StatementContext
+    );
 
-      // @ISSUE-1 - workaround
-      if (
-        this.declarationStack.length !== 0 &&
-        !this.isLastDeclarationApplied &&
-        child instanceof TerminalNode &&
-        child.getText() === '}'
-      ) {
-        this.declarationList.push([...this.declarationStack]);
-        this.isLastDeclarationApplied = true;
-      }
-    });
+    const propertyDeclarations = statements?.map((statementCtx) =>
+      statementCtx.children?.filter(
+        (c) => c instanceof PropertyDeclarationContext
+      )
+    );
 
-    // ctx.statement_list()
+    propertyDeclarations?.map((statement) =>
+      statement?.map((propertyDeclaration) => {
+        this.visit(propertyDeclaration);
+      })
+    );
+
+    this.declarationList.push([...this.declarationStack]);
+    this.declarationStack = [];
+
+    const rulesets = statements?.map((statementCtx) =>
+      statementCtx.children?.filter((c) => c instanceof RulesetContext)
+    );
+
+    rulesets?.map((statement) =>
+      statement?.map((ruleset) => {
+        this.visit(ruleset);
+      })
+    );
   };
 
-  convertToCSS = (): CSSType =>
-    this.declarationList
-      .map(
-        (declaration, i) =>
-          this.selectorList.reverse()[i].join(' ').replace(' &.', '.') +
-          ' ' +
-          '{' +
-          '\n' +
-          '\t' +
-          declaration.join('\n\t') +
-          '\n' +
-          '}'
+  convertToCSS = (): CSSType => {
+    this.selectorList.reverse();
+
+    return this.declarationList
+      .map((declaration, i) =>
+        declaration.length
+          ? this.selectorList[i].join(' ').replaceAll(' &', '') +
+            ' ' +
+            '{' +
+            '\n' +
+            '\t' +
+            declaration.join('\n\t') +
+            '\n' +
+            '}'
+          : ''
       )
-      .join('\n');
+      .join('\n\n');
+  };
 
   getOutput = (): CSSType => {
     return this.convertToCSS();
